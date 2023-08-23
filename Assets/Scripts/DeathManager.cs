112e647f68950557c42958b00bb3893ffdb3042a
Frieda <meine@mail.de>
2023-08-23 17:37:28 +0200

    death manager

--- Assets/Scripts/DeathManager.cs
+++ Assets/Scripts/DeathManager.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathManager : MonoBehaviour
 {
 
     private void Update()
     {
-        if (transform.position.y < -10)
+        if (transform.position.y < -50)
         {
             print("Du hast leider verloren!");
+            SceneManager.LoadScene(2);
 
         }
     }
